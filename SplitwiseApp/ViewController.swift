ad8fa77b42620ee90c94f722d86c4835be0270fb
leerachel5 <leerachel481@gmail.com>
2024-08-31 20:05:39 -0700

    initial commit

---
+++ SplitwiseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitwiseApp
+//
+//  Created by Rachel Lee on 8/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
